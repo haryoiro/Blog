8e839d952f34caa2e946022dd3556aec917416a3
haryoiro <mizusecocolte@gmail.com>
2020-12-09 18:34:26 +0900

    ⚙ Auto Build on GitHub Actions ⚙

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,12 @@ if (process.env.ENVIRONMENT !== 'production') {
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `HaryoiroTechBlog`,
+    description: ``,
     author: `@gatsbyjs`,
+  },
+  flags: {
+    PRESERVE_WEBPACK_CACHE: true,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
