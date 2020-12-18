c1e5c0be00ca6bc3aae694ee3ecb62815498de58
haryoiro <mizusecocolte@gmail.com>
2020-12-18 14:17:24 +0900

    cache

---
+++ config/theme.js
@@ -0,0 +1,19 @@
+const colors = {
+  black: {
+    base: '#333438',
+    light: '#4b4e57',
+    lighter: '#696d77',
+    blue: '#2e3246',
+  },
+}
+const transition = {
+  easeInOutCubic: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+  easeOutBack: 'cubic-bezier(0.175, 0.885, 0.32, 1.275)',
+  duration: '0.4s',
+}
+const theme = {
+  colors,
+  transition,
+}
+
+export default theme
