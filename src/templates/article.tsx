5d314e4f3936cb89b632b4d50f7c2a9baf44f696
haryoiro <mizusecocolte@gmail.com>
2020-12-20 20:10:33 +0900

    Change query name

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -15,23 +15,28 @@ import SEO from '../components/SEO'
 import MDComponents from '../components/MDXComponents'
 
 // type declarations
-import { ArticlePostQuery } from '../../types/graphql-types'
+import { ArticleBySlugQuery } from '../../types/graphql-types'
 
 type Props = {
-  data: ArticlePostQuery
+  data: ArticleBySlugQuery
 }
 
 // 
 const ArticlePost: React.FC<Props> = ({ data }) => {
-  const articles = data.articles
+  const { article: {
+    title,
+    node: { childMdx: { 
+      body
+    } }
+  }} = data
 
   return (
     <Layout>
-      <SEO title={articles.title} type="article" />
+      <SEO title={title} type="article" />
       <MDXProvider components={MDComponents}>
-        <h2 id='article-title'>{articles?.title}</h2>
+        <h2 id='article-title'>{title}</h2>
         <article id='article-body'>
-          <MDXRenderer>{articles.body.child.body}</MDXRenderer>
+          <MDXRenderer>{body}</MDXRenderer>
         </article>
       </MDXProvider>
     </Layout>
@@ -39,29 +44,27 @@ const ArticlePost: React.FC<Props> = ({ data }) => {
 }
 
 export default ArticlePost
-//e7c7b004-2332-50e0-908c-0cd005d56469
-export const query = graphql`
-  query BlogPostBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-        author
-      }
-    }
 
-    articles: contentfulArticles(id: { eq: $id }) {
-      id
-      slug
+export const query = graphql`
+query ArticleBySlug($slug: String!) {
+  site {
+    siteMetadata {
       title
-      features
-      tags
-      createdAt(fromNow: true)
-      body: childContentfulArticlesBodyTextNode {
-        child: childMdx {
-          body
-          text: excerpt
-        }
-      }
+      author
     }
   }
+  article: contentfulArticles(slug: {eq: $slug}) {
+    slug
+    title
+
+    createdAt(formatString: "MMMM/DD/YY HH:MM")
+    node: childContentfulArticlesBodyTextNode {
+      childMdx {
+        body
+      }
+    }
+    updatedAt(fromNow: true)
+  }
+}
+
 `
