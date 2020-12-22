6d8515907e8c706eb2ea33391d77040ab4845aa3
haryoiro <mizusecocolte@gmail.com>
2020-12-22 13:12:26 +0900

    Generate Page for ArticleListPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,27 +1,59 @@
 const path = require('path')
 
-exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
-  const blogPostTemplate = path.resolve('./src/templates/article.tsx')
 
   const result = await graphql(`
   {
-    allContentfulArticles {
+    allContentfulArticles(
+      limit: 1000,
+      sort: {fields: createdAt, order: DESC}) {
       edges {
         node {
           slug
         }
       }
     }
-  }`).catch((error) => { throw error })
+  }
+  `)
 
+  // GraphQLクエリ実行中エラー
+  if (result.errors) {
+    reporter.panicOnBuild('Error while running GraphQL query.')
+    return
+  }
+
+  // すべての記事
   const posts = result.data.allContentfulArticles.edges
 
-  posts.forEach((edge) => {
-    const subDir = `/blog/${edge.node.slug}`
+  // 一覧ページで表示する記事の本数
+  const postsPerPage = 6
+
+  // 生成される記事一覧ページのページ数
+  const numPages = Math.ceil(posts.length / postsPerPage)
+
+  // 記事一覧ページの作成
+  Array.from({ length: numPages }).forEach((_, i) => {
+    // 配列のindexをもとにページ番号を割り当て。
+    const pagePath = i === 0 ? '/blog/1/' : `/blog/${i + 1}/`
     createPage({
-      path: subDir,
-      component: blogPostTemplate,
+      path: pagePath,
+      component: path.resolve('./src/templates/articleList.tsx'),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
+  })
+
+  // それぞれの記事の詳細
+  posts.forEach((edge) => {
+    const pagePath = `/blog/${edge.node.slug}/`
+    createPage({
+      path: pagePath,
+      component: path.resolve('./src/templates/article.tsx'),
       context: {
         slug: edge.node.slug,
       },
