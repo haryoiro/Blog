f77d195e590d08a721c28a350a18b08bcd2f272b
haryoiro <mizusecocolte@gmail.com>
2021-01-12 19:19:42 +0900

    Add Syntax Highlighting #34

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { MDXProviderComponentsProp } from '@mdx-js/react'
+import '../styles/prism.css'
 
 export type CustomComponents = React.FC<{
   children: React.ReactElement & React.ReactNode & undefined & null
 }>
 
-const Pre: CustomComponents = ({ children }) => <pre>{children}</pre>
+const Pre: CustomComponents = ({ children }) => <pre className="notranslate">{children}</pre>
 
 const customComponents: MDXProviderComponentsProp = {
   Pre,
